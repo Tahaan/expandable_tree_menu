58115cc95e5fdce5c09ed5f146b7990b5b323036
Johan Hartzenberg <johan@webmaster.co.za>
2021-04-22 12:15:40 +0200

    Removed another completed To-Do item

--- lib/src/defaults.dart
+++ lib/src/defaults.dart
@@ -2,13 +2,6 @@ import 'package:flutter/material.dart';
 
 const Icon DEFAULT_CLOSED_TWISTY = Icon(Icons.expand_more);
 const Icon DEFAULT_OPENED_TWISTY = Icon(Icons.expand_less);
-const Icon DEFAULT_LEAFNODE_TWISTY = Icon(
-  Icons.circle,
-  color: Colors.transparent, // Spacer to ensure alignment of leaf nodes
-);
-
-// TODO: Get rid of the place holder in favour of another way of aligning
-//       leaf nodes with non-leaf nodes.
 
 enum TwistyState { root, open, closed, leaf }
 
